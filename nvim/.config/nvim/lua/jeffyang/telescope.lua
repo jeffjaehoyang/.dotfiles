94870faf2402fad452790044c2ae7ec7e9d4f7d5
Jeff Yang <jho.yang96@gmail.com>
2022-12-17 01:42:50 -0500

    lua rewrite nvim configs

--- nvim/.config/nvim/lua/jeffyang/telescope.lua
+++
@@ -1,4 +0,0 @@
--- separate between work and personal machine
-vim.api.nvim_set_keymap("n", "<C-p>", [[<cmd>Telescope find_files<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-f>", [[<cmd>Telescope live_grep<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>b", [[<cmd> Telescope buffers<CR>]], {noremap = true, silent = true})
