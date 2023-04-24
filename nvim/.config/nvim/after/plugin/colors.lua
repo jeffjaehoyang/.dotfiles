dab15eb484be5cb656d5a04f73c342905b22c9cf
Jeff Yang <jho.yang96@gmail.com>
2023-04-23 23:00:27 -0400

    stuff

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,9 +1,11 @@
 function ColorMyPencils(color)
-    color = color or "gruvbox"
+    color = color or "kanagawa"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = '#1560C0' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { fg = 'None', bg = 'None' })
 
     vim.api.nvim_set_hl(0, 'SignColumn', { bg = "none" })
     vim.api.nvim_set_hl(0, 'DiagnosticError', { bg = "none" })
--- nvim/.config/nvim/lua/jeffyang/packer.lua
+++ nvim/.config/nvim/lua/jeffyang/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
     use { "mikesmithgh/gruvsquirrel.nvim" }
     use { "novasenco/nokto" }
     use { "AlxHnr/clear_colors" }
+    use { "rebelot/kanagawa.nvim" }
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'nvim-treesitter/playground' }
